dbc3ee245ded00b38cd1dc3c25b76b798a3c5355
Seth Todd <seth.todd@outlook.com>
2020-01-26 02:38:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badges|
     puts badges
   end
-    assign_rooms.each do |rooms|
+    assign_rooms(attendees).each do |rooms|
       puts rooms
   end
 end
