9c2ba1c23065dbdfa49840d2bb2f8e0161ff8e07
Seth Todd <seth.todd@outlook.com>
2020-01-26 02:24:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |name, index|
-    room_assignments << [puts "Hello, #{name}! You'll be assigned to room #{index+1}!"]
+    room_assignments << {puts "Hello, #{name}! You'll be assigned to room #{index+1}!"}
   end
   room_assignments
 end
