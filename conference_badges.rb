f5b8b0e923243a2b6b6f401f948bae76b9d022c9
Seth Todd <seth.todd@outlook.com>
2020-01-26 02:00:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,13 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+
+attendees = ["Joe", "Timmy", "Kris", "Jenna"]
+
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |name|
     badges << badge_maker(name)
   end
+  badges
 end
