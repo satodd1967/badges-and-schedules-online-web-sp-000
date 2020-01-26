bac02a00e810afa0474fe32b2c05fc4750e3169a
Seth Todd <seth.todd@outlook.com>
2020-01-26 02:20:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,4 +16,5 @@ def assign_rooms(attendees)
   attendees.each_with_index do |name, index|
     room_assignments << puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
+  room_assignments
 end
